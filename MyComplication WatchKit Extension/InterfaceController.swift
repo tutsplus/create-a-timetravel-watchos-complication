7cf13a3e84f43022dab515cbbaf580333b481e55
Derek Jensen <jensendp@kreatived.com>
2016-10-21 10:26:35 -0500

    Initial Commit

---
+++ MyComplication WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MyComplication WatchKit Extension
+//
+//  Created by Derek Jensen on 10/21/16.
+//  Copyright © 2016 Derek Jensen. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ MyComplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyComplication
+//
+//  Created by Derek Jensen on 10/21/16.
+//  Copyright © 2016 Derek Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
