0dbdf3c2564ec16304c279f6aecfbe7b6cf53065
Derek Jensen <jensendp@kreatived.com>
2016-10-21 12:11:22 -0500

    Initial commit

--- MyComplication WatchKit Extension/ComplicationController.swift
+++ MyComplication WatchKit Extension/ComplicationController.swift
@@ -11,6 +11,8 @@ import ClockKit
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
     
+    let timeLineText = ["Grab some Breakfast", "Drive to Work", "Meet with Boss", "Get a big Raise!"]
+    
     // MARK: - Timeline Configuration
     
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
@@ -18,11 +20,18 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     }
     
     func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
+        
+        let currentDate = Date()
+        
+        handler(currentDate)
     }
     
     func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
+        
+        let currentDate = Date()
+        let endDate = currentDate.addingTimeInterval(4 * 60 * 60)
+        
+        handler(endDate)
     }
     
     func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
@@ -33,7 +42,14 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        handler(nil)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm"
+        
+        let timeString = dateFormatter.string(from: Date())
+        let entry = createTimeEntry(headerText: timeString, bodyText: timeLineText[0], date: Date())
+        
+        handler(entry)
     }
     
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
@@ -43,7 +59,24 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         // Call the handler with the timeline entries after to the given date
-        handler(nil)
+        
+        var timeLineEntryArray = [CLKComplicationTimelineEntry]()
+        var nextDate = Date(timeIntervalSinceNow: 1 * 60 * 60)
+        
+        for index in 1...3 {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "hh:mm"
+            
+            let timeString = dateFormatter.string(from: nextDate)
+            let entry = createTimeEntry(headerText: timeString, bodyText: timeLineText[index], date: nextDate)
+            
+            timeLineEntryArray.append(entry)
+            
+            nextDate = nextDate.addingTimeInterval(1 * 60 * 60)
+        }
+        
+        
+        handler(timeLineEntryArray)
     }
     
     // MARK: - Placeholder Templates
@@ -52,5 +85,21 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         // This method will be called once per supported complication, and the results will be cached
         handler(nil)
     }
+    
+    
+    func createTimeEntry(headerText: String, bodyText: String, date: Date) -> CLKComplicationTimelineEntry {
+        let template = CLKComplicationTemplateModularLargeStandardBody()
+        template.headerTextProvider = CLKSimpleTextProvider(text: headerText)
+        template.body1TextProvider = CLKSimpleTextProvider(text: bodyText)
+        
+        let entry = CLKComplicationTimelineEntry(date: date, complicationTemplate: template)
+        
+        return entry
+    }
+    
+    
+    
+    
+    
     
 }
